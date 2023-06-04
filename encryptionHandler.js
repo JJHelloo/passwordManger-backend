efad71af48adf50549d8de75e036135d75ac8197
JJHelloo <jujohnson@csumb.edu>
2023-06-04 16:56:20 -0700

    added routes handled encrytion of the passwords and more..

--- encryptionHandler.js
+++ encryptionHandler.js
@@ -1,35 +1,49 @@
-const crypto = require("crypto");
-// will be changed to master password future devlopment
-const secret = "pppppppppppppppppppppppppppppppp";
+const crypto = require('crypto');
 
-const encrypt = (password) => {
-  const iv = Buffer.from(crypto.randomBytes(16));
-  const cipher = crypto.createCipheriv("aes-256-ctr", Buffer.from(secret), iv);
+const encrypt = (data, masterPassword) => {
+  const salt = crypto.randomBytes(16).toString('hex');
 
-  const encryptedPassword = Buffer.concat([
-    cipher.update(password),
-    cipher.final(),
-  ]);
+  return new Promise((resolve, reject) => {
+    crypto.pbkdf2(masterPassword, salt, 100000, 32, 'sha512', (err, derivedKey) => {
+      if (err) {
+        reject(err);
+      } else {
+        const iv = crypto.randomBytes(16);
+        const cipher = crypto.createCipheriv('aes-256-ctr', derivedKey, iv);
 
-  return {
-    iv: iv.toString("hex"),
-    password: encryptedPassword.toString("hex"),
-  };
+        let encryptedData = cipher.update(data, 'utf8', 'hex');
+        encryptedData += cipher.final('hex');
+
+        const encryptedObject = {
+          salt,
+          iv: iv.toString('hex'),
+          data: encryptedData
+        };
+
+        resolve(encryptedObject);
+      }
+    });
+  });
 };
 
-const decrypt = (encryption) => {
-  const decipher = crypto.createDecipheriv(
-    "aes-256-ctr",
-    Buffer.from(secret),
-    Buffer.from(encryption.iv, "hex")
-  );
-
-  const decryptedPassword = Buffer.concat([
-    decipher.update(Buffer.from(encryption.password, "hex")),
-    decipher.final(),
-  ]);
-
-  return decryptedPassword.toString();
+const decrypt = (encryption, masterPassword) => {
+  return new Promise((resolve, reject) => {
+    crypto.pbkdf2(masterPassword, encryption.salt, 100000, 32, 'sha512', (err, derivedKey) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const decipher = crypto.createDecipheriv('aes-256-ctr', derivedKey, Buffer.from(encryption.iv, 'hex'));
+          let decryptedData = decipher.update(encryption.data, 'hex', 'utf8');
+          decryptedData += decipher.final('utf8');
+          resolve(decryptedData);
+        } catch (err) {
+          reject(err);
+        }
+      }
+    });
+  });
 };
+
 
 module.exports = { encrypt, decrypt };
